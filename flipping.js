805df9fd026cd2c63d1c138be6ecc2e6e8a0c72a
Ladan Torkzaban <99889241+ladantork@users.noreply.github.com>
2024-02-26 14:16:10 +0000

    Done with advance solution

--- flipping.js
+++ flipping.js
@@ -1,7 +1,3 @@
 function flipBooleans(bools) {
-    const flippedArray = [];
-    for(let i=0;i < bools.length; i++){
-        flippedArray.push(!bools[i]);
-    }
-    return flippedArray
+    return bools.map((bool)=>!bool);
 }
